ce19d390c990ad7516cd340236aff1ff36d639d1
liberadzkih <liberadzkih@gmail.com>
2020-02-04 12:18:09 +0100

    Added Client-Server file sending - just for test

---
+++ Server/src/sample/FileServer.java
@@ -0,0 +1,19 @@
+package sample;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class FileServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Socket socket = serverSocket.accept();
+        FileInputStream fileInputStream = new FileInputStream("C:\\Users\\Hlibe\\OneDrive\\Pulpit\\po2_project_files\\server_files\\user1\\stars.png");
+        byte[] bytes = new byte[20000];
+        fileInputStream.read(bytes, 0, bytes.length);
+        OutputStream outputStream = socket.getOutputStream();
+        outputStream.write(bytes, 0, bytes.length);
+    }
+}
